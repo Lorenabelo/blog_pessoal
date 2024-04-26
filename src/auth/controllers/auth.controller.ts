ce615aebcad1a827c27674b0e35e90b7b37c830d
Lorena Belo <loore.belo@gmail.com>
2024-04-26 09:55:12 -0300

    fix: ajuste no controller usuario

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -3,13 +3,13 @@ import { AuthService } from '../services/auth.service';
 import { UsuarioLogin } from '../entities/usuariologin.entity';
 import { LocalAuthGuard } from '../guard/local-auth.guard';
 
-@Controller('/usuario')
+@Controller('/usuarios')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @UseGuards(LocalAuthGuard)
   @HttpCode(HttpStatus.OK)
-  @Post('logar')
+  @Post('/logar')
   async login(@Body() user: UsuarioLogin): Promise<any> {
     return this.authService.login(user);
   }
